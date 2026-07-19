63c5123535a442503cd5bc46e98ea3ba79e97eb4
Amir Ahmed Siddiqui <113359516+Am1rAhmed@users.noreply.github.com>
2026-07-19 16:33:21 +0530

    Add files via upload

---
+++ STRING/131_Palind_Parti.py
@@ -0,0 +1,36 @@
+
+# Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+ 
+
+# Example 1:
+
+# Input: s = "aab"
+# Output: [["a","a","b"],["aa","b"]]
+# Example 2:
+
+# Input: s = "a"
+# Output: [["a"]]
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 16
+# s contains only lowercase English letters.
+
+
+class Solution:
+    def partition(self, s):
+        res = []
+        def find(s, path, res):
+            if not s:
+                res.append(path)
+                return
+            for i in range(1, len(s) + 1):
+                if s[:i] == s[:i][::-1]:
+                    find(s[i:], path + [s[:i]], res)
+        find(s, [], res)
+        return res
+    
+S = Solution().partition("aab")
+print(S)
