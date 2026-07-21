786d236c026ba401440ec31194fc017639522268
Amir Ahmed Siddiqui <113359516+Am1rAhmed@users.noreply.github.com>
2026-07-21 18:01:09 +0530

    Add files via upload

---
+++ ARRAY/169_Majority_ele.py
@@ -0,0 +1,45 @@
+
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. 
+# You may assume that the majority element always exists in the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+ 
+
+# Constraints:
+
+# n == nums.length
+# 1 <= n <= 5 * 104
+# -109 <= nums[i] <= 109
+# The input is generated such that a majority element will exist in the array.
+ 
+
+# Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+class Solution:
+    def majorityElement(self, nums):
+        nums.sort()
+        count = 0
+        maj_ele = nums[0]
+        for i in range(len(nums)-1):
+            count += 1
+            if nums[i] != nums[i+1] and count <= (len(nums)//2):
+                count = 0
+                maj_ele = nums[i+1]
+            
+        return maj_ele
+
+S = Solution().majorityElement([2,2,1,1,1,2,2])
+print(S)
+
+
